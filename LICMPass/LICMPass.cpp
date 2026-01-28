11e97499e84c7b61eb69af0256bd4a394b110d76
bocadelic <boledelic05@gmail.com>
2026-01-28 18:00:08 +0100

    mapping load instruction

--- LICMPass/LICMPass.cpp
+++ LICMPass/LICMPass.cpp
@@ -1,16 +1,44 @@
 #include "llvm/IR/Function.h"
 #include "llvm/Pass.h"
 #include "llvm/Support/raw_ostream.h"
+#include "llvm/Analysis/LoopInfo.h"
+#include "llvm/Analysis/LoopPass.h"
+#include "llvm/IR/Instruction.h"
+
+#include <vector>
+#include <unordered_map>
 
 using namespace llvm;
 
 namespace {
-    struct LICMPass : public FunctionPass {
-        static char ID;
-        LICMPass() : FunctionPass(ID) {}
+    struct LICMPass : public LoopPass {
+        std::vector<BasicBlock *> LoopBasicBlocks;
+        Instruction* InsertBefore;
+        std::unordered_map<Value*, Value*> VariableMap;
 
-        bool runOnFunction(Function &F) override {
-            errs() << "Hello World!\n";
+        static char ID;
+        LICMPass() : LoopPass(ID) {}
+
+        void variableMapping() {
+            Function *Parent = LoopBasicBlocks[0]->getParent();
+            for (BasicBlock &BB: *Parent) {
+                for (Instruction &I : BB) {
+                    if (auto *LI = dyn_cast<LoadInst>(&I)) {
+                        VariableMap[LI] = LI->getOperand(0);
+                    }
+                }
+            }
+        }
+
+        bool runOnLoop(Loop *L, LPPassManager&) override {
+            LoopBasicBlocks = L->getBlocksVector();
+            BasicBlock *PreHeader = L->getLoopPreheader();
+            if (!PreHeader)
+                return false;
+            InsertBefore = PreHeader->getTerminator();
+
+            variableMapping();
+
             return false;
         }
     };
